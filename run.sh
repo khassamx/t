12b584e09fb53237d56dc4bce86432eeb8e9963b
khassamx <khasamxx@gmail.com>
2026-03-08 15:41:00 -0300

    Actualizar run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Carpeta del proyecto
 cd ~/whatsapp-backup
-
-# Ejecutar Python backup
 python backup.py
 
-# Subir automáticamente a GitHub
 git add .
 git commit -m "Backup automático $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
 
-echo "Backup y GitHub completado con éxito"
+echo "Backup y GitHub completado"
